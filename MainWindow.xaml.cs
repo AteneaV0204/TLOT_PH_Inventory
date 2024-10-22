1cdf125dc747f18b4c5fa6981e2ab3575bdeb8b6
Atenea <avadillog0504@gmail.com>
2024-10-22 23:16:21 +0200

    Primer commit con imagenes incluidas

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventario_Zelda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Reloj_Click(object sender, RoutedEventArgs e)
+        {
+            ImageBrush brush = new();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/img/relojA_1.png"));
+
+            Reloj.Background = brush;
+        }
+    }
+}
